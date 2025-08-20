3b9f97b478e015930888098135b216080756c785
Richard H Lee <ricardohenrylee@gmail.com>
2025-08-20 18:23:03 +0100

    error handling, tabs

--- background.js
+++ background.js
@@ -1,18 +1,41 @@
 "use strict";
 
+const wrap = _function => {
+  const errorStack = new Error();
+  return (..._arguments) => _function.apply(null, _arguments)
+    .catch(error => {
+      errorStack.message = error.toString().replace(/^Error: /, "");
+      console.log(errorStack);
+    });
+}
+
 let tabActive = null;
 
 browser.browserAction.setBadgeBackgroundColor({color: [0, 0, 0, 0]});
 browser.browserAction.setBadgeText({text: "🔴"});
 
-browser.browserAction.onClicked.addListener(tab => {
+browser.browserAction.onClicked.addListener(wrap(async tab => {
   if (tabActive) {
     browser.browserAction.setBadgeText({text: null, tabId: tabActive});
     browser.browserAction.setBadgeText({text: "🔴"});
     tabActive = null;
-  } else {
-    tabActive = tab.id;
-    browser.browserAction.setBadgeText({text: "🟠"});
-    browser.browserAction.setBadgeText({text: "🟢", tabId: tabActive});
+  } else await browser.tabs.executeScript({
+    code: `browser.runtime.sendMessage({
+      tab: ${tab.id},
+      path: prompt(${await browser.storage.local.get('path').path})
+    });`
+  });
+}));
+
+browser.runtime.onMessage.addListener(wrap(async (message, sender) => {
+  switch (message) {
+    case null: break;
+    case "":
+      await browser.storage.local.clear('path');
+      break;
+    default:
+      tabActive = message.tab;
+      browser.browserAction.setBadgeText({text: "🟠"});
+      browser.browserAction.setBadgeText({text: "🟢", tabId: tabActive});
   }
-});
+}));
